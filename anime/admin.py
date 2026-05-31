502c196065c85d683c9f436004ecf74e468aeff1
hashmashdashcrash <epichashem@hotmail.com>
2026-05-31 19:10:06 +0100

    Add anime and watchlist models

--- anime/admin.py
+++ anime/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Anime, WatchlistItem
 
-# Register your models here.
+
+@admin.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genres', 'rating', 'episodes')
+    search_fields = ('title', 'genres')
+
+
+@admin.register(WatchlistItem)
+class WatchlistItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'anime', 'added_at')
+    search_fields = ('user__username', 'anime__title')
--- anime/models.py
+++ anime/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Anime(models.Model):
+    title = models.CharField(max_length=200)
+    genres = models.CharField(max_length=300, blank=True)
+    rating = models.FloatField(default=0)
+    episodes = models.IntegerField(default=0)
+    synopsis = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class WatchlistItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'anime')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.anime.title}"
